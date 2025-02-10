ab4ca6a203b69898fcbc3a0a32169901e128f164
Shresta-Voruganti <22bd1a053z@gmail.com>
2025-02-10 23:14:42 +0530

    day 3

--- a1/10.02.25/sp4.java
+++ a1/10.02.25/sp4.java
@@ -1,1 +1,33 @@
-//
+import java.util.*;
+
+public class sp4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(maxsubk(arr, n, k));
+        sc.close();
+    }
+
+    public static List<Integer> maxsubk(int[] arr, int n, int k) {
+        List<Integer> result = new ArrayList<>();
+        List<Integer> calc = new ArrayList<>();
+
+        for(int i = 0; i < k; i++) {
+            calc.add(arr[i]);
+        }
+        result.add(Collections.max(calc));
+
+        for(int i = k; i < n; i++) {
+            calc.remove(0);
+            calc.add(arr[i]);
+            result.add(Collections.max(calc));
+        }
+
+        return result;
+    }
+}
