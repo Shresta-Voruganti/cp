9a32a81011b5ff556443ce3cf9584504e64ac05a
Shresta-Voruganti <22bd1a053z@gmail.com>
2025-02-18 23:05:13 +0530

    day 7

--- a1/14.02.25/ap16.java
+++ a1/14.02.25/ap16.java
@@ -1,1 +1,44 @@
-//
+import java.util.*;
+
+public class ap16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sorting(n, arr);
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+        sc.close();
+    }
+
+    public static void sorting(int n, int[] arr) {
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+
+        while(mid <= high) {
+            if(arr[mid] == 0) {
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            }
+            else if(arr[mid] == 1) {
+                mid++;
+            }
+            else {
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int p1, int p2) {
+        int temp = arr[p1];
+        arr[p1] = arr[p2];
+        arr[p2] = temp;
+    }
+}
