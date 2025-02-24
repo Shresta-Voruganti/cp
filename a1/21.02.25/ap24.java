997f72795d838c005726a2bf741b6c9efef2e6bb
Shresta-Voruganti <22bd1a053z@gmail.com>
2025-02-24 13:06:39 +0530

    day 11

--- a1/21.02.25/ap24.java
+++ a1/21.02.25/ap24.java
@@ -1,1 +1,29 @@
-//
+import java.util.*;
+
+public class ap24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] times = new int[n];
+        for(int i = 0; i < n; i++) {
+            times[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        System.out.println(trips(n, times, k));
+        sc.close();
+    }
+
+    public static int trips(int n, int[] times, int k) {
+        int sum = 0;
+        int t = 0;
+        while(sum < k) {
+            t++;
+            sum = 0;
+            for(int i = 0; i < n; i++) {
+                sum += t/times[i];
+            }
+        }
+
+        return t;
+    }
+}
