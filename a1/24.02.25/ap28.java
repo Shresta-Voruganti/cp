78a7014974992d687846086cf9508c18d2455eee
Shresta-Voruganti <22bd1a053z@gmail.com>
2025-03-03 16:33:12 +0530

    day 12

--- a1/24.02.25/ap28.java
+++ a1/24.02.25/ap28.java
@@ -1,1 +1,76 @@
-//
+import java.util.*;
+
+public class ap28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int T = sc.nextInt();
+        int V = sc.nextInt();
+        int[] screenplays = new int[n];
+        for(int i = 0; i < n; i++) {
+            screenplays[i] = sc.nextInt();
+        }
+        // int T = sc.nextInt();
+        // int V = sc.nextInt();
+        System.out.println(refill(n, screenplays, T, V));
+        sc.close();
+    }
+
+    public static int refill(int n, int[] screenplays, int T, int V) {
+        int trivi = 0;
+        int vamshi = n - 1;
+        int refills = 0;
+        int refT = T;
+        int refV = V;
+
+        while(trivi <= vamshi) {
+            if(trivi == vamshi) {
+                if(refT >= screenplays[trivi]) {
+                    refT -= screenplays[trivi];
+                    trivi++;
+                }
+                else if(refV >= screenplays[vamshi]) {
+                    refV -= screenplays[vamshi];
+                    vamshi--;
+                }
+                else {
+                    refT = T;
+                    refV = V;
+                    if(refT > refV) {
+                        refT -= screenplays[trivi];
+                        trivi++;
+                    }
+                    else {
+                        refV -= screenplays[vamshi];
+                        vamshi--;
+                    }
+                    refills++;
+                }
+            }
+            else {
+                if(refT >= screenplays[trivi]) {
+                    refT -= screenplays[trivi];
+                    trivi++;
+                }
+                else {
+                    refT = T;
+                    refT -= screenplays[trivi];
+                    trivi++;
+                    refills++;
+                }
+                if(refV >= screenplays[vamshi]) {
+                    refV -= screenplays[vamshi];
+                    vamshi--;
+                }
+                else {
+                    refV = V;
+                    refV -= screenplays[vamshi];
+                    vamshi--;
+                    refills++;
+                }
+            }
+        }
+
+        return refills;
+    }
+}
