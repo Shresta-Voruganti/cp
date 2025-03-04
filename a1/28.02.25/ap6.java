4af1da6426fa0589201a074a370bae16e660f955
Shresta-Voruganti <22bd1a053z@gmail.com>
2025-03-04 23:25:46 +0530

    day 15

--- a1/28.02.25/ap6.java
+++ a1/28.02.25/ap6.java
@@ -1,1 +1,45 @@
-//
+import java.util.*;
+
+public class ap6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String num = sc.next();
+        int k = sc.nextInt();
+        System.out.println(remKdig(num, k));
+        sc.close();
+    }
+
+    public static String remKdig(String num, int k) {
+        int n = num.length();
+        if(k >= n) {
+            return "0";
+        }
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(char digit : num.toCharArray()) {
+            while(!stack.isEmpty() && k > 0 && stack.peekLast() > digit) {
+                stack.pollLast();
+                k--;
+            }
+            stack.addLast(digit);
+        }
+
+        while(k > 0 && !stack.isEmpty()) {
+            stack.pollLast();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while(!stack.isEmpty()) {
+            sb.append(stack.pollFirst());
+        }
+
+        while(sb.length() > 1 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
--- a1/28.02.25/ap7.java
+++ a1/28.02.25/ap7.java
@@ -1,1 +1,76 @@
+import java.util.*;
+
+public class ap7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(maxsum(n, arr));
+        sc.close();
+    }
+
+    public static int maxsum(int n, int[] arr) {
+        if(n == 1) {
+            return 0;
+        }
+
+        int sum = 0;
+        int currprod = 0;
+        for(int i = 0; i < n; i++) {
+            sum += arr[i];
+            currprod += (i * arr[i]);
+        }
+
+        int maxprod = currprod;
+        for(int i = 1; i < n; i++) {
+//            currprod = currprod + sum - arr[n - i] - (n - 1) * arr[n - i];
+            currprod = currprod + sum - n * arr[n - i];
+            maxprod = Math.max(maxprod, currprod);
+        }
+
+        return maxprod;
+    }
+
+//    public static int maxsum(int n, int[] arr) {
+//        if(n == 1) {
+//            return 0;
+//        }
 //
+//        int k = 0;
+//        int prod = 0;
+//        int maxprod = 0;
+//        for(int i = 0; i < n; i++) {
+//            reverse(arr, 0, n - 1);
+//            reverse(arr, 0, k - 1);
+//            reverse(arr, k, n - 1);
+//            prod = mul(n, arr);
+//            maxprod = Math.max(prod, maxprod);
+//            k++;
+//        }
+//
+//        return maxprod;
+//    }
+//
+//    public static void reverse(int[] arr, int start, int end) {
+//        while(start < end) {
+//            int temp = arr[start];
+//            arr[start] = arr[end];
+//            arr[end] = temp;
+//            start++;
+//            end--;
+//        }
+//    }
+//
+//    public static int mul(int n, int[] arr) {
+//        int prod = 0;
+//
+//        for(int i = 0; i < n; i++) {
+//            prod += (i * arr[i]);
+//        }
+//
+//        return prod;
+//    }
+}
