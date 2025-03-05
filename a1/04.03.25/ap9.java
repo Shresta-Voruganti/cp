119ebc925b8ca21a7f5f893f9ca2f565f87a8158
Shresta-Voruganti <22bd1a053z@gmail.com>
2025-03-06 00:17:34 +0530

    day 17

--- a1/04.03.25/ap9.java
+++ a1/04.03.25/ap9.java
@@ -1,1 +1,67 @@
-//
+import java.util.*;
+
+public class ap9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] grid = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(orangesRotting(grid));
+        sc.close();
+    }
+
+    static int[][] dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    public static int orangesRotting(int[][] grid) {
+        int time = 0; //to rot all oranges
+        int m = grid.length;
+        int n = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        int freshCount = 0;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 2) {
+                    q.offer(new int[]{i, j});
+                }
+                else if(grid[i][j] == 1) {
+                    freshCount++;
+                }
+            }
+        }
+
+        if(freshCount == 0) {
+            return 0;
+        }
+
+        while(!q.isEmpty()) {
+            int qsize = q.size();
+            boolean isRotted = false;
+
+            for(int i = 0; i < qsize; i++) {
+                int[] curr = q.poll();
+                int x = curr[0];
+                int y = curr[1];
+                for(int[] dir : dirs) {
+                    int newx = x + dir[0];
+                    int newy = y + dir[1];
+
+                    while(newx >= 0 && newx < m && newy >= 0 && newy < n && grid[newx][newy] == 1) {
+                        grid[newx][newy] = 2;
+                        q.offer(new int[]{newx, newy});
+                        freshCount--;
+                        isRotted = true;
+                    }
+                }
+            }
+            if(isRotted) {
+                time++;
+            }
+        }
+        return freshCount == 0 ? time : -1;
+    }
+}
