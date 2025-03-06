a1f378913a2a524a7873c7e001375583f98e9b46
Shresta-Voruganti <22bd1a053z@gmail.com>
2025-03-06 21:40:05 +0530

    day 14

--- a1/27.02.25/ap4.java
+++ a1/27.02.25/ap4.java
@@ -1,1 +1,42 @@
-//
+import java.util.*;
+
+public class ap4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(minops(str));
+        sc.close();
+    }
+
+    public static int minops(String s) {
+        Stack<Character> stack = new Stack<>();
+        int ans = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if(ch == '[') {
+                stack.push(ch);
+            }
+            else {
+                if(i < s.length() - 1 && s.charAt(i + 1) == ']') {
+                    i++;
+                }
+                else {
+                    ans++;
+                }
+
+                if(stack.isEmpty()) {
+                    ans++;
+                }
+                else {
+                    stack.pop();
+                }
+            }
+        }
+
+        ans += stack.size()  * 2;
+
+        return ans;
+    }
+}
