81c75101fd36e3b2f7afa767d01c60a08c66adc2
Shresta-Voruganti <22bd1a053z@gmail.com>
2025-03-08 00:22:35 +0530

    day 12

--- a1/24.02.25/ap27.java
+++ a1/24.02.25/ap27.java
@@ -1,1 +1,31 @@
-//
+import java.util.*;
+
+public class ap27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(impbor(nums));
+        sc.close();
+    }
+
+    public static int impbor(int[] nums) {
+        Set<Integer> s1 = new HashSet<>();
+
+        for(int num : nums) {
+            s1.add(num);
+        }
+
+        int power = 1;
+        while(s1.contains(power)) {
+            power = power << 1;
+            //left shift operator - gets multiplied by 2
+            //similar to `power = power * 2`
+        }
+
+        return power;
+    }
+}
